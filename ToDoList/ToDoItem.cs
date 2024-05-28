83986d4eb7be0ef7438d954b944804c4edb683d5
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-05-28 04:54:41 +0300

    Added ToDoItem class

---
+++ ToDoList/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList
+{
+    public class ToDoItem
+    {
+        public string Title {  get; set; }
+        public string? Description {  get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+    }
+}
